98a0187027f22ebf52c12dbe3efda3f40bdffece
unknown <francofyzz15@gmail.com>
2023-03-19 20:38:36 -0300

    (api/index.js)-fixing a bug

--- api/index.js
+++ api/index.js
@@ -1,18 +1,24 @@
 require('dotenv').config({ path: './.env' });
-const { API_PORT } = process.env;
+const { 
+    API_PORT,
+    MONGO_NAME,
+    MONGO_HOST,
+    PASSWORD_MONGO,
+    MONGO_USER,
+} = process.env;
 const server = require('./src/server.js');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(`${MONGO_NAME}://${MONGO_USER}:${PASSWORD_MONGO}@${MONGO_HOST}/?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(()=>{
-    console.log('database connected.');
-    server.listen(API_PORT, (error) => {
-        if(error) throw new Error(error);
-        console.log('api listening at port: ', API_PORT);
+    .then(() => {
+        console.log('database connected.');
+        server.listen(API_PORT, (error) => {
+            if (error) throw new Error(error);
+            console.log('api listening at port: ', API_PORT);
+        });
+    }).catch((error) => {
+        console.error(error);
     });
-}).catch((error) => {
-    console.error(error);
-});
