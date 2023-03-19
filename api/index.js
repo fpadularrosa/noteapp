bf652e6b976a916ade9fabdcec0240008f4da533
unknown <francofyzz15@gmail.com>
2023-03-19 20:55:50 -0300

    (api/index.js)-fixing a bug

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ require('dotenv').config({ path: './.env' });
 const server = require('./src/server.js');
 const mongoose = require('mongoose');
 
-mongoose.connect(`${process.env.MONGODB_URI}`, {
+mongoose.connect(`${process.env.MONGO_URL}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
